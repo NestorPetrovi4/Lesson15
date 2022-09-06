093caf164b6edfb9e44f4864b06a08f3ce81f714
Oleg Nesterov <oleg9988@rambler.ru>
2022-09-06 12:05:28 +0300

    Добавление функций описывающих поведение класса MagicBox

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,2 +1,30 @@
-public class MagicBox {
+import java.util.Random;
+
+public class MagicBox<T> {
+    protected T[] items;
+
+    public MagicBox(int length) {
+        this.items = (T[]) new Object[length];
+    }
+
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T pick() {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException("Коробка заполнена не полностью осталось свободно " + (items.length - i + 1) + " ячейки");
+            }
+        }
+        Random random = new Random();
+        int randomInt = random.nextInt(items.length);
+        return items[randomInt];
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int lengthBoxString = 3;
+        int lengthBoxInteger = 4;
+        MagicBox<String> boxString = new MagicBox<>(lengthBoxString);
+        MagicBox<Integer> boxInteger = new MagicBox<>(lengthBoxInteger);
+
+        boxString.add("Зайчик");
+        boxString.add("Шарик");
+        boxString.add("Савок");
+        boxString.add("Фонарик");
+        System.out.println(boxString.pick());
+
+        boxInteger.add(3);
+        boxInteger.add(6);
+        boxInteger.add(18);
+        boxInteger.add(22);
+        boxInteger.add(65);
+        boxInteger.add(7);
+        System.out.println(boxInteger.pick());
     }
 }
